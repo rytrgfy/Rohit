9833ca3e4f6d70df45173943aed75b5785cb5a76
Rohit Kumar Sahoo <rohitkumarsahoo2002@gmail.com>
2025-02-24 10:25:01 +0530

    php Shorthand syntax
    done shorthand syntax

---
+++ demo/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>hello</h1>
+    <?
+    echo "hello world <br><br><br><br>";
+    echo 2+3;
+    ?>
+</body>
+
+</html>
