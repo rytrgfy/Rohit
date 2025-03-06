19051ff2468e5e5dc3633421b5c9af5aabb38c40
Rohit Kumar Sahoo <rohitkumarsahoo2002@gmail.com>
2025-03-06 13:19:35 +0530

    update login.php
    login.php done

--- loginandsignup/login.php
+++ loginandsignup/login.php
@@ -1,3 +1,39 @@
 <?
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
 include "dbconn.php";
+$email = $password = "";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $email = isset($_POST['email']) ? $_POST['email'] : null;
+    $password = isset($_POST['password']) ? $_POST['password'] : null;
+
+    // for getting id query 
+    $fetch_id = "SELECT ID FROM `signup` WHERE email = '$email'";
+    $get_id = $conn->query($fetch_id);
+    $id_data = $get_id->fetch_assoc();
+
+    // based on id from email i'm passing value to see
+    $fetch_data_sql = "SELECT * FROM signup WHERE id = '{$id_data['ID']}' ";
+    $result = $conn->query($fetch_data_sql);
+
+    if (!$result) {
+        die("Query failed: " . $conn->error);
+    }
+
+    $data = $result->fetch_assoc();
+
+    if (!$data) {
+        die("No record found with ");
+    }
+
+    if ($data['EMAIL'] === $email && $data['PASSWORD'] === $password) {
+        echo "<script>alert('login success')</script>";
+    } else {
+        echo "<script>alert('Login failed! Use correct email or password.');window.location.href = 'login.html';</script>";
+
+    }
+
+
+}
 ?>
